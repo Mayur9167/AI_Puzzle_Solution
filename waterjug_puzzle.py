b01601c38a237b14e149ee5a40cde35d45fb42e2
Mayur9167 <chodankarmayur1@gmail.com>
2023-08-23 16:51:51 +0530

    water jug completed

--- waterjug_puzzle.py
+++ waterjug_puzzle.py
@@ -1,6 +1,6 @@
 import time
 from collections import deque
-
+import heapq
 
 
 print("Breadth-First Search")
@@ -128,3 +128,112 @@ end_time = time.time()
 
 execution_time = end_time - start_time
 print("Execution Time:", execution_time, "seconds")
+
+
+
+print("A* Search")
+
+def water_jug_astar(capacity_a, capacity_b, target):
+    def heuristic(state):
+        return abs(state[0] - target[0]) + abs(state[1] - target[1])
+    
+    visited = set()
+    open_set = [(heuristic((0, 0)), 0, (0, 0))]  # (f-value, g-value, state)
+    
+    while open_set:
+        _, g, state = heapq.heappop(open_set)
+        a, b = state
+        
+        if state == target:
+            print(target)
+            return True
+        
+        visited.add(state)
+        print(state)
+        
+        # Fill jug A
+        if (capacity_a, b) not in visited:
+            heapq.heappush(open_set, (heuristic((capacity_a, b)) + g + 1, g + 1, (capacity_a, b)))
+        
+        # Fill jug B
+        if (a, capacity_b) not in visited:
+            heapq.heappush(open_set, (heuristic((a, capacity_b)) + g + 1, g + 1, (a, capacity_b)))
+        
+        # Empty jug A
+        if (0, b) not in visited:
+            heapq.heappush(open_set, (heuristic((0, b)) + g + 1, g + 1, (0, b)))
+        
+        # Empty jug B
+        if (a, 0) not in visited:
+            heapq.heappush(open_set, (heuristic((a, 0)) + g + 1, g + 1, (a, 0)))
+        
+        # Pour from A to B
+        pour = min(a, capacity_b - b)
+        if (a - pour, b + pour) not in visited:
+            heapq.heappush(open_set, (heuristic((a - pour, b + pour)) + g + 1, g + 1, (a - pour, b + pour)))
+        
+        # Pour from B to A
+        pour = min(b, capacity_a - a)
+        if (a + pour, b - pour) not in visited:
+            heapq.heappush(open_set, (heuristic((a + pour, b - pour)) + g + 1, g + 1, (a + pour, b - pour)))
+    
+    return False
+
+
+# Example usage
+capacity_a = 4
+capacity_b = 3
+target = (2, 0)
+
+start_time = time.time()
+print("A* Result:", water_jug_astar(capacity_a, capacity_b, target))
+end_time = time.time()
+
+execution_time = end_time - start_time
+print("Execution Time:", execution_time, "seconds")
+
+
+
+
+print("\nHill Climbing")
+
+def water_jug_hill_climbing(capacity_a, capacity_b, target):
+    def heuristic(state):
+        return abs(state[0] - target[0]) + abs(state[1] - target[1])
+    
+    current_state = (0, 0)
+    current_heuristic = heuristic(current_state)
+    
+    while current_state != target:
+        neighbors = [
+            (capacity_a, current_state[1]),
+            (current_state[0], capacity_b),
+            (0, current_state[1]),
+            (current_state[0], 0),
+            (min(current_state[0] + current_state[1], capacity_a), max(0, current_state[1] - (capacity_a - current_state[0]))),
+            (max(0, current_state[0] - (capacity_b - current_state[1])), min(current_state[1] + current_state[0], capacity_b))
+        ]
+        
+        best_neighbor = min(neighbors, key=heuristic)
+        
+        if heuristic(best_neighbor) >= current_heuristic:
+            break
+        
+        current_state = best_neighbor
+        current_heuristic = heuristic(current_state)
+        print(current_state)
+    
+    return current_state == target
+
+# Example usage
+capacity_a = 4
+capacity_b = 3
+target = (2, 0)
+
+start_time = time.time()
+print("Hill Climbing Result:", water_jug_hill_climbing(capacity_a, capacity_b, target))
+end_time = time.time()
+
+execution_time = end_time - start_time
+print("Execution Time:", execution_time, "seconds")
+
