1f31ce61bfbc1cb33294bb01773a995c5bcaf149
Mayur9167 <chodankarmayur1@gmail.com>
2023-08-25 23:14:42 +0530

    documented

--- robo_cleaner.py
+++ robo_cleaner.py
@@ -1,9 +1,14 @@
 from queue import PriorityQueue
 from queue import Queue
-import heapq
 
 class RoboCleaner:
     def __init__(self, grid):
+        """
+        Initialize the RoboCleaner object with the provided grid layout.
+
+        Args:
+            grid (list[list[str]]): The grid layout containing '0' (clean tiles) and 'X' (obstacles).
+        """
         self.grid = grid
         self.rows = len(grid)
         self.cols = len(grid[0])
@@ -11,9 +16,23 @@ class RoboCleaner:
         self.position = (0, 0)
         
     def is_valid(self, row, col):
+        """
+        Check if the given row and column indices are within the valid grid boundaries
+        and the corresponding tile is not an obstacle.
+
+        Args:
+            row (int): The row index.
+            col (int): The column index.
+
+        Returns:
+            bool: True if the tile is valid, False otherwise.
+        """
         return 0 <= row < self.rows and 0 <= col < self.cols and self.grid[row][col] != 'X'
     
     def bfs(self):
+        """
+        Perform Breadth-First Search (BFS) cleaning starting from the current position.
+        """
         visited = set()
         queue = Queue()
         queue.put(self.position)
@@ -32,6 +51,9 @@ class RoboCleaner:
                     queue.put((new_row, new_col))
     
     def dfs(self):
+        """
+        Perform Depth-First Search (DFS) cleaning starting from the current position.
+        """
         visited = set()
         stack = [self.position]
         
@@ -49,6 +71,9 @@ class RoboCleaner:
                     stack.append((new_row, new_col))
     
     def a_star(self):
+        """
+        Perform A* search cleaning to move from the current position to the goal.
+        """
         start = self.position
         goal = (self.rows - 1, self.cols - 1)
         
@@ -77,6 +102,9 @@ class RoboCleaner:
                     open_list.put((new_cost, (new_row, new_col)))
 
     def hill_climbing(self):
+        """
+        Perform Hill Climbing cleaning to move from the current position while optimizing the heuristic.
+        """
         current_node = self.position
         while True:
             neighbors = []
@@ -95,11 +123,22 @@ class RoboCleaner:
             current_node = best_neighbor
     
     def heuristic(self, node):
+        """
+        Calculate the heuristic value for a given node as the Manhattan distance to the goal.
+
+        Args:
+            node (tuple): The node's (row, col) coordinates.
+
+        Returns:
+            int: The heuristic value.
+        """
         goal = (self.rows - 1, self.cols - 1)
         return abs(node[0] - goal[0]) + abs(node[1] - goal[1])
 
-    
     def print_grid(self):
+        """
+        Print the current grid layout.
+        """
         for row in self.grid:
             print(" ".join(row))
 
@@ -133,6 +172,3 @@ robo_cleaner.hill_climbing()
 
 print("\nGrid after cleaning:")
 robo_cleaner.print_grid()
-
-
-
